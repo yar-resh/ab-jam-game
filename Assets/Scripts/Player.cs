18bab51015882e8046cd99689e2dc69e76f8fb59
Maryna Reshetnyk <maryna.reshetnyk@gmail.com>
2021-11-06 20:08:18 +0200

    hero walk and jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5.0f;
+    [SerializeField]
+    private bool _grounded = false;
+    private bool _resetJump = false;
+    [SerializeField]
+    private float _speed = 2.5f;
+
+    private PlayerAnimation _anim;
+    private SpriteRenderer _sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<PlayerAnimation>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        _grounded = IsGrounded();
+        _anim.Jump(!_grounded);
+
+        if (move != 0) _sprite.flipX = move < 0;
+
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && _grounded)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+        }
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+        _anim.Move(move);
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1 << 8);
+        return (hitInfo.collider != null && !_resetJump);
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void Jump(bool jump)
+    {
+        _anim.SetBool("Jump", jump);
+    }
+}
