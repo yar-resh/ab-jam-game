a91b4885d51a501d7cd1ae1e74fe0c1cd4d14722
yar-resh <reshetnik.yaroslav@gmail.com>
2021-11-07 16:20:34 +0200

    Add animation for enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public EnemyPath Path;
+    public float Speed = 2f;
+    public float Delta = 0.2f;
+
+    private Vector3 direction = Vector3.right;
+    private SpriteRenderer sprite;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Path != null)
+        {
+            var currentPosition = transform.localPosition;
+            if (Mathf.Abs((Path.EndPosition.x - currentPosition.x)) <= Delta && direction.Equals(Vector3.right))
+            {
+                direction = Vector3.left;
+                sprite.flipX = true;
+            } 
+            else if (Mathf.Abs((Path.StartPosition.x - currentPosition.x)) <= Delta && direction.Equals(Vector3.left))
+            {
+                direction = Vector3.right;
+                sprite.flipX = false;
+            }
+
+            transform.Translate(direction * Speed * Time.deltaTime);
+            
+        }
     }
 }
---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    public Vector2 StartPosition;
+    public Vector2 EndPosition;
+}
